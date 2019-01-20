243ac5053a76025131f0d063965130142c313d51
katrin <andreevak53@gmail.com>
2019-01-20 19:54:30 +0000

    add

---
+++ index.js
@@ -0,0 +1,32 @@
+// 1.По нажатию на кнопку "btn-msg" должен появиться алерт с тем текстом который находится в атрибуте data-text у кнопки. +++++
+
+
+let btn = document.getElementById('btn-msg');
+
+function clicked() {
+   let text = document.getElementById('btn-msg').dataset.text; 
+    alert(text); 
+}
+btn.addEventListener('click', clicked);
+
+
+// 2. При наведении указателя мыши на "btn-msg", кнопка становится красной; когда указатель мыши покидает кнопку, 
+// она становится прежнего цвета. Цвет менять можно через добавление класса.
+
+ function onmove() {
+    document.getElementById("btn-msg").style.backgroundColor = '#ff0000';
+}
+ btn.addEventListener('mouseover', onmove);
+ 
+ btn.addEventListener('mouseout', function() {
+     document.getElementById('btn-msg').style.backgroundColor = '';
+ });
+ 
+
+// 3. При нажатии на любой узел документа показать в элементе с id=tag имя тега нажатого элемента.
+
+document.body.onclick=e=>document.querySelector("#tag").textContent = 'Tag: '+e.target.tagName;
+
+// 4. При нажатии на кнопку btn-generate добавлять в список ul элемент списка Li с текстом Item + порядковый номер Li по списку,
+// т.е Item 3, Item 4 и т.д 
+
